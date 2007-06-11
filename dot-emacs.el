f16d94626e2b0aba206fcb61697f90066d7be56d
Aleix Conchillo Flaque <aleix@member.fsf.org>
2007-06-11 09:12:02 +0200

    update dot emacs file

--- dot-emacs.el
+++ dot-emacs.el
@@ -3,7 +3,8 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
-)
+ '(jde-ant-enable-find t)
+ '(jde-ant-read-target t))
 
 ;; Path variables
 (defconst emacs-init-dir  (expand-file-name "~/Library/Emacs/")
@@ -27,18 +28,19 @@
 (add-to-list 'load-path emacs-packages-dir)
 
 ;; Load modules
-(init-load-file "globals")        ;; Global initializations
 (init-load-file "colors")         ;; Emacs color themes
 (init-load-file "devel")          ;; General development
 (init-load-file "edit")           ;; Edit/Typing customisations
 (init-load-file "func")           ;; Lisp functions
-(init-load-file "jde")            ;; Java Development Environment
+(init-load-file "globals")        ;; Global initializations
 (init-load-file "keys")           ;; Key settings
-;(init-load-file "macavity")       ;; MacAvity
-(init-load-file "skels")          ;; File skeletons
+;(init-load-file "jde")            ;; Java Development Environment
+(init-load-file "lisp")            ;; LISP
+(init-load-file "macavity")       ;; MacAvity
+(init-load-file "muse")           ;; Emaacs Muse Mode
+;(init-load-file "skels")          ;; File skeletons
 (init-load-file "sgml")           ;; SGML stuff
 (init-load-file "tex")            ;; TeX initialization
-(init-load-file "www")            ;; Web related stuff
 
 ;; color faces
 (custom-set-faces
