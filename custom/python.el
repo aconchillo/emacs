eebbe952c2d2a530144582a5df47c9711cfa3478
Aleix Conchillo Flaque <aleix@member.fsf.org>
2011-02-03 10:55:05 +0100

    automatically guess rope project

--- custom/python.el
+++ custom/python.el
@@ -6,4 +6,7 @@
 ;; Automatically enable company mode for Python
 (add-hook 'python-mode-hook (lambda () (company-mode +1)))
 
+;; Automatically find rope project
+(setq ropemacs-guess-project 't)
+
 ;;; python.el ends here
