42b72ea882bfd998c5d93155fb4ccaa1a2c25968
Aleix Conchillo Flaque <aconchillo@gmail.com>
2011-06-06 01:00:39 +0200

    colors: load system color-theme (emacs-goodies-el)

--- custom/colors.el
+++ custom/colors.el
@@ -1,9 +1,11 @@
 ;;; colors.el --- Setup Emacs color theme
 
-;; Color themes
-(add-to-list 'load-path (expand-file-name "color-theme/" emacs-packages-dir))
-
 (require 'color-theme)
+
+(eval-after-load "color-theme"
+  '(progn
+     (color-theme-initialize)
+     (color-theme-hober)))
 
 ;; User defined colors
 (add-to-list 'load-path (expand-file-name "color-theme/" emacs-init-dir))
