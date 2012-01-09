d977c482c37166c968c1d38590d42e64f69bee2b
Aleix Conchillo Flaque <aleix@oblong.com>
2012-01-09 15:28:44 -0800

    safe themes now use sha-256 instead of sha-1

--- custom/colors.el
+++ custom/colors.el
@@ -4,7 +4,7 @@
 (add-to-list 'custom-theme-load-path (expand-file-name "themes/" emacs-init-dir))
 
 ;; Treat pumba as a safe theme
-(setq custom-safe-themes (quote ("85298aefde7415fbbee7a509e2ab30a6e4478130" default)))
+(setq custom-safe-themes (quote ("efd0a33925f986b31953377623e5bdd4129277e3e0227f47919c3512481a8fef" default)))
 
 ;; Theme
 (load-theme 'pumba)
