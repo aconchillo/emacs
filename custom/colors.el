e71128c5787fbac15aea48b1198bffeab79145ff
Aleix Conchillo Flaque <aconchillo@gmail.com>
2012-01-09 11:54:05 -0800

    add custom theme load path

--- custom/colors.el
+++ custom/colors.el
@@ -1,5 +1,8 @@
 ;;; colors.el --- Setup Emacs color theme
 
+;; Add custom themes path
+(add-to-list 'custom-theme-load-path (expand-file-name "themes/" emacs-init-dir))
+
 ;; Treat pumba as a safe theme
 (setq custom-safe-themes (quote ("85298aefde7415fbbee7a509e2ab30a6e4478130" default)))
 
