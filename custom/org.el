2a01bbb136949f142ab4d620e2b2db0dfba1398f
Aleix Conchillo Flaque <aconchillo@gmail.com>
2012-02-06 13:02:28 -0800

    start using john wiegley org mode config

--- custom/org.el
+++ custom/org.el
@@ -1,25 +1,76 @@
 ;;; org.el ---  Setup Org environment
 
-;; Indent tree
-;(setq org-startup-indented t)
-(setq org-hide-leading-stars t)
+(require 'org-install)
 
-;; Adds CLOSED entry when TODO is DONE.
-(setq org-log-done 'note)
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
 
-(setq org-file-apps (quote ((auto-mode . emacs)
-                            ("\\.x?html?\\'" . default)
-                            ("\\.doc\\'" . default)
-                            ("\\.odt\\'" . default)
-                            ("\\.ods\\'" . default)
-                            ("\\.pdf\\'" . default)
-                            ("\\.xls\\'" . default))))
+(eval-after-load "org-agenda"
+  '(progn
+     (define-prefix-command 'org-todo-state-map)
 
-(setq org-agenda-files (quote ("~/Work/org/scew.org" "~/Work/org/work-ice.org")))
-(setq org-archive-default-command (quote org-archive-to-archive-sibling))
+     (define-key org-mode-map "\C-cx" 'org-todo-state-map)
+
+     (define-key org-todo-state-map "x"
+       #'(lambda nil (interactive) (org-todo "CANCELLED")))
+     (define-key org-todo-state-map "d"
+       #'(lambda nil (interactive) (org-todo "DONE")))
+     (define-key org-todo-state-map "f"
+       #'(lambda nil (interactive) (org-todo "DEFERRED")))
+     (define-key org-todo-state-map "l"
+       #'(lambda nil (interactive) (org-todo "DELEGATED")))
+     (define-key org-todo-state-map "s"
+       #'(lambda nil (interactive) (org-todo "STARTED")))
+     (define-key org-todo-state-map "w"
+       #'(lambda nil (interactive) (org-todo "WAITING")))
+
+     (define-key org-agenda-mode-map "\C-n" 'next-line)
+     (define-key org-agenda-keymap "\C-n" 'next-line)
+     (define-key org-agenda-mode-map "\C-p" 'previous-line)
+     (define-key org-agenda-keymap "\C-p" 'previous-line)))
+
+(require 'remember)
+
+(add-hook 'remember-mode-hook 'org-remember-apply-template)
+
+(define-key global-map [(control meta ?r)] 'remember)
+
+(custom-set-variables
+ '(org-agenda-files (quote ("~/src/org-mode/oblong.org")))
+ '(org-default-notes-file "~/src/org-mode/notes.org")
+ '(org-agenda-ndays 7)
+ '(org-deadline-warning-days 14)
+ '(org-agenda-show-all-dates t)
+ '(org-agenda-skip-deadline-if-done t)
+ '(org-agenda-skip-scheduled-if-done t)
+ '(org-agenda-start-on-weekday nil)
+ '(org-reverse-note-order t)
+ '(org-fast-tag-selection-single-key (quote expert))
+ '(org-agenda-custom-commands
+   (quote (("d" todo "DELEGATED" nil)
+           ("c" todo "DONE|DEFERRED|CANCELLED" nil)
+           ("w" todo "WAITING" nil)
+           ("W" agenda "" ((org-agenda-ndays 21)))
+           ("A" agenda ""
+            ((org-agenda-skip-function
+              (lambda nil
+                (org-agenda-skip-entry-if (quote notregexp) "\\=.*\\[#A\\]")))
+             (org-agenda-ndays 1)
+             (org-agenda-overriding-header "Today's Priority #A tasks: ")))
+           ("u" alltodo ""
+            ((org-agenda-skip-function
+              (lambda nil
+                (org-agenda-skip-entry-if (quote scheduled) (quote deadline)
+                                          (quote regexp) "\n]+>")))
+             (org-agenda-overriding-header "Unscheduled TODO entries: "))))))
+ '(org-remember-store-without-prompt t)
+ '(org-remember-templates
+   (quote (("Notes" ?n "* %u %?" "~/src/org-mode/notes.org" "Notes")
+           ("Oblong" ?o "* TODO %?\n  %u" "~/src/org-mode/oblong.org" "Tasks"))))
+ '(remember-annotation-functions (quote (org-remember-annotation)))
+ '(remember-handler-functions (quote (org-remember-handler))))
 
 ;;; org.el ends here
