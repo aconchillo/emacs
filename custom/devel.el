7227d508194fb8d2647d5bcba38f3a6d63ca6d87
Aleix Conchillo Flaque <aconchillo@gmail.com>
2012-09-03 23:20:49 -0700

    enable doxymacs

--- custom/devel.el
+++ custom/devel.el
@@ -56,13 +56,13 @@
 ;(yas/initialize)
 
 ;; Doxymacs (already loaded in Debian)
-;(add-to-list 'load-path (expand-file-name "doxymacs/lisp/" emacs-packages-dir))
-;(require 'doxymacs)
+(add-to-list 'load-path (expand-file-name "doxymacs/lisp/" emacs-packages-dir))
+(require 'doxymacs)
 
-;(add-hook 'c-mode-common-hook 'doxymacs-mode)
-;(defun my-doxymacs-font-lock-hook ()
-;  (if (or (eq major-mode 'c-mode) (eq major-mode 'c++-mode))
-;      (doxymacs-font-lock)))
-;(add-hook 'font-lock-mode-hook 'my-doxymacs-font-lock-hook)
+(add-hook 'c-mode-common-hook 'doxymacs-mode)
+(defun my-doxymacs-font-lock-hook ()
+  (if (or (eq major-mode 'c-mode) (eq major-mode 'c++-mode))
+      (doxymacs-font-lock)))
+(add-hook 'font-lock-mode-hook 'my-doxymacs-font-lock-hook)
 
 ;;; devel.el ends here
