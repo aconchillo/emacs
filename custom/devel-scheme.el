8186c277584514c3669f153f75ea42a182e205c9
Aleix Conchillo Flaque <aconchillo@gmail.com>
2013-04-15 10:54:41 -0700

    use gesier from git

--- custom/devel-scheme.el
+++ custom/devel-scheme.el
@@ -1,6 +1,14 @@
 ;;; devel-scheme.el ---  Setup Scheme environment
 
+;; General Scheme mode
+(setq scheme-program-name "guile")
+
+;; ParEdit
+(add-hook 'scheme-mode-hook 'paredit-mode)
+
 ;; Geiser
+(load-file "~/etc/emacs/elisp/geiser/elisp/geiser.el")
+
 (setq geiser-repl-history-filename "~/.emacs.d/geiser-history")
 
 ;; ParEdit
