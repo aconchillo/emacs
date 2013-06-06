e0dbbf2ab6707eec179e4cd1b6f3e0e71047309d
Aleix Conchillo Flaque <aleix@oblong.com>
2013-06-06 11:08:14 -0700

    org: add support for appointment notifications

--- custom/org.el
+++ custom/org.el
@@ -77,4 +77,47 @@
  '(remember-annotation-functions (quote (org-remember-annotation)))
  '(remember-handler-functions (quote (org-remember-handler))))
 
+;; Agenda notifications
+;; from http://emacs-fu.blogspot.com/2009/11/showing-pop-ups.html
+
+;; warn 15 min in advance
+(setq appt-message-warning-time 10)
+
+;; show notificationnalso in modeline
+(setq appt-display-mode-line t)
+
+;; use our function to display notifications
+(setq appt-display-format 'window)
+
+;; active appt (appointment notification)
+(appt-activate 1)
+
+;; time display is required for this...
+(display-time)
+
+;; update appt each time agenda opened
+(add-hook 'org-finalize-agenda-hook 'org-agenda-to-appt)
+
+(defun djcb-popup (title msg &optional icon sound)
+  "Show a popup if we're on X, or echo it otherwise; TITLE is the
+title of the message, MSG is the context. Optionally, you can
+provide an ICON and a sound to be played"
+  (interactive)
+  (when sound (shell-command
+               (concat "mplayer -really-quiet " sound " 2> /dev/null")))
+  (if (eq window-system 'x)
+    (shell-command (concat "notify-send "
+                           (if icon (concat "-i " icon) "")
+                           " '" title "' '" msg "'"))
+    ;; text only version
+    (message (concat title ": " msg))))
+
+;; our little façade-function for djcb-popup
+(defun djcb-appt-display (min-to-app new-time msg)
+  (djcb-popup (format "Appointment in %s minute(s)" min-to-app) msg
+              "/usr/share/icons/gnome/32x32/status/appointment-soon.png"
+              "/usr/share/sounds/ubuntu/stereo/message.ogg"))
+
+(setq appt-disp-window-function (function acf-appt-display))
+
 ;;; org.el ends here
