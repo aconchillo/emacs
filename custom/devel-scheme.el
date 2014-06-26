8ffee4189305003f557f20df0c59bd147876b0d4
Aleix Conchillo Flaqué <aleix@oblong.com>
2014-06-26 13:00:38 +0200

    user elpa's geiser

--- custom/devel-scheme.el
+++ custom/devel-scheme.el
@@ -7,8 +7,6 @@
 (add-hook 'scheme-mode-hook 'paredit-mode)
 
 ;; Geiser
-(load-file "~/etc/emacs/elisp/geiser/elisp/geiser.el")
-
 (setq geiser-repl-history-filename "~/.emacs.d/geiser-history")
 
 ;; ParEdit
