51005fb353a0f39fee84576b5f2a5e13b19c5292
Aleix Conchillo Flaqué <aleix@oblong.com>
2016-12-08 16:51:12 -0800

    add solarized color

--- custom/colors.el
+++ custom/colors.el
@@ -4,9 +4,11 @@
 (add-to-list 'custom-theme-load-path (expand-file-name "themes/" emacs-init-dir))
 
 ;; Treat pumba as a safe theme
-(setq custom-safe-themes (quote ("fa90d6964d1a8aabce590a3418a055b3360adba60055363077a2a906683df520" default)))
+(setq custom-safe-themes (quote ("fa90d6964d1a8aabce590a3418a055b3360adba60055363077a2a906683df520"
+                                 "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328"
+                                 default)))
 
 ;; Theme
-(load-theme 'pumba)
+(load-theme 'sanityinc-solarized-dark)
 
 ;;; colors.el ends here
