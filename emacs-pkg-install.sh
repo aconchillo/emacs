cbdbc9ed3ce7bcf501e4a27031a3e5d34b9449ee
Aleix Conchillo Flaqué <aleix@oblong.com>
2017-01-26 23:34:57 -0800

    new setup script. happy hacking!

---
+++ emacs-pkg-install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [ $# -eq 0 ]
+then
+  echo "Usage: `basename $0` <package> ..."
+  exit 1
+fi
+
+emacs --batch --eval "(defconst pkgs-to-install '($*))" -l emacs-pkg-install.el
---
+++ setup.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+./emacs-pkg-install.sh \
+  auto-complete \
+  color-theme-sanityinc-solarized \
+  flx-ido \
+  magit \
+  markdown-mode \
+  muse \
+  paradox \
+  projectile \
+  smex \
+  #
+
+echo
+echo "Checking for .emacs ..."
+if ! test -f $HOME/.emacs
+then
+    echo "Creating $HOME/.emacs ..."
+    ln -s etc/emacs/dot-emacs.el $HOME/.emacs
+fi
+
+echo "Checking for .ercinfo ..."
+if ! test -f $HOME/.ercinfo
+then
+    echo "Creating $HOME/.ercinfo ..."
+    ln -s etc/emacs/dot-ercinfo.el $HOME/.ercinfo
+fi
+
+echo
+echo "Happy hacking!"
+echo
