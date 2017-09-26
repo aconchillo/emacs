9110ec4015fafd0ce2bf207d9841ab862f66097c
Aleix Conchillo Flaqué <aleix@oblong.com>
2017-09-26 09:49:40 -0700

    elpa: remove aleix packages

--- custom/elpa.el
+++ custom/elpa.el
@@ -6,9 +6,6 @@
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 
-(add-to-list 'package-archives
-             '("oblong" . "http://hacks-galore.org/aleix/emacs/packages/") t)
-
 ;; Fix HTTP1/1.1 problems
 (setq url-http-attempt-keepalives nil)
 
