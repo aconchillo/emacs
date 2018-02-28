b979d61e5e3eaf81ce39f371d691436159a41002
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2018-02-28 00:06:26 -0800

    use .m for octave files

--- custom/devel-octave.el
+++ custom/devel-octave.el
@@ -1,8 +1,8 @@
 ;;; devel-octave.el ---  Setup Octave environment
 
 ;; Set octave for .m files
-;; (setq auto-mode-alist
-;;       (cons '("\\.m$" . octave-mode) auto-mode-alist))
+(setq auto-mode-alist
+      (cons '("\\.m$" . octave-mode) auto-mode-alist))
 
 (add-hook 'octave-mode-hook
           (lambda ()
