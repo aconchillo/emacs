228636239b5771621d801eb2c105e9784c18d8eb
Aleix Conchillo Flaque <aleix@oblong.com>
2019-02-21 17:47:58 -0800

    setup: update package repos

--- emacs-pkg-install.el
+++ emacs-pkg-install.el
@@ -7,14 +7,7 @@
 (require 'package)
 
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
-
-(add-to-list 'package-archives
-             '("marmalade" . "https://marmalade-repo.org/packages/") t)
-
-;; Disable security checks so we can download https repos with
-;; self-signed certificate.
-(setq network-security-level 'low)
+             '("melpa" . "https://melpa.org/packages/") t)
 
 ;; Fix HTTP1/1.1 problems
 (setq url-http-attempt-keepalives nil)
