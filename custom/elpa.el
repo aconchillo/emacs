db2cc5bb07e2681411e8a8799cce6e393b1e5e7f
Aleix Conchillo Flaque <aleix@oblong.com>
2019-02-21 16:55:14 -0800

    elpa: remove marmalade and update melpa url

--- custom/elpa.el
+++ custom/elpa.el
@@ -1,10 +1,7 @@
 ;;; packages.el --- Setup package management
 
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
-
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+             '("melpa" . "https://melpa.org/packages/") t)
 
 ;; Fix HTTP1/1.1 problems
 (setq url-http-attempt-keepalives nil)
