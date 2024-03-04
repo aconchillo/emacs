e874afd576d3226f7ac28391beec0d75358c60b9
Aleix Conchillo Flaqué <aconchillo@gmail.com>
2024-03-04 10:38:55 -0800

    add tree-sitter-rust

--- init.el
+++ init.el
@@ -206,6 +206,7 @@
           (make "https://github.com/alemuller/tree-sitter-make")
           (markdown "https://github.com/ikatyang/tree-sitter-markdown")
           (python "https://github.com/tree-sitter/tree-sitter-python")
+          (rust "https://github.com/tree-sitter/tree-sitter-rust")
           (toml "https://github.com/tree-sitter/tree-sitter-toml")
           (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
           (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
